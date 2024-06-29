10c5d11571f67fa3eaa3045aa2642692976ed3a9
Ryoforge <ryoforge20@gmail.com>
2024-06-29 17:18:58 +0530

    Restart all

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-// App.js
+import React from 'react'
 
-import React, { useState, useEffect } from 'react';
-import SplashScreen from './components/splashscreen';
-import './App.css'; // Example for CSS import
-
-const App = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        // Simulate an async operation (e.g., fetching data)
-        setTimeout(() => {
-            setLoading(false); // Set loading to false when your data is ready
-        }, 2000); // Adjust the delay as needed
-    }, []);
-
-    return (
-        <div className="App">
-            {loading ? (
-                <SplashScreen />
-            ) : (
-                <div className="main-content">
-                    {/* Your main application content */}
-                    <h1>Welcome to My App</h1>
-                </div>
-            )}
-        </div>
-    );
-};
-
-export default App;
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
