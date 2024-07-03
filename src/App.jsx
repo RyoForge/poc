9db9eda9e6cb595b6eb627a3bc31ed409ff154e6
Ryoforge <ryoforge20@gmail.com>
2024-07-03 22:46:06 +0530

    Update: Home.jsx Bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/Home"
 const App = () => {
   return (
     <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+    <div className="pt-2 overflow-hidden">
         <Home/>
     </div>
     
